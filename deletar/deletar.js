6c564f7c713754ebb6efb53dc9c1be832226d6b2
Laura Barauna <lauravbarauna@gmail.com>
2026-03-01 11:21:23 -0300

    Melhora semantica da pagina de deletar, e estrutura toda sua regra

---
+++ deletar/deletar.js
@@ -0,0 +1,61 @@
+const form = document.getElementById("myForm");
+const id = document.getElementById("idParaRemover");
+
+const modal = document.getElementById("modal");
+const textoModal= document.getElementById("frase-modal");
+const fecharModal = document.getElementById("fecharModal");
+
+const abrirModal = (frase) => {
+    textoModal.textContent = frase;
+    modal.classList.remove("hidden");
+    recarregarForm();
+};
+
+const fechar = () => {
+    modal.classList.add("hidden");
+};
+
+const recarregarForm = () => {
+    id.value = "";
+}
+
+const removerColab = async () => {
+    try {
+        const res = await fetch(`https://69a37967611ecf5bfc22e438.mockapi.io/lauravbarauna/provasuficiencia/employees/${id.value}`, {
+            method: "DELETE"
+        });
+
+        if (!res.ok) {
+            if (res.status = 404) {
+                abrirModal(`Colab com id ${id.value} não encontrado!`);
+            }
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
+
+        abrirModal(`Colab com id ${id.value} deletado com sucesso!`);
+        console.log(`Colab com id ${id.value} deletado!`);
+    } catch (error) {
+        console.error(`Erro ao deletar colab com id ${id.value}: ${error}`);
+    }
+
+}
+
+const formValido = () => {
+    if (!id.value.trim()) {
+        abrirModal("Informe um ID para remover.");
+        return false;
+    }
+    return true;
+};
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (!formValido()) {
+        return;
+    }
+    
+    removerColab();
+})
+
+fecharModal.addEventListener("click", fechar);
